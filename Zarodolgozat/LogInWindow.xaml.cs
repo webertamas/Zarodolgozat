2ba1f71554b2ed1eda4c0ef714e23619f4c781bd
Tamás Wéber <webertamas@hotmail.com>
2017-09-03 20:50:01 +0200

    Add project files.

---
+++ Zarodolgozat/LogInWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Zarodolgozat
+{
+    /// <summary>
+    /// Interaction logic for LogInWindow.xaml
+    /// </summary>
+    public partial class LogInWindow : Window
+    {
+        public LogInWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInOkButtonClick(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(userNameTextBox.Text))
+            {
+                MessageBox.Show("Nem írta be a felhasználónevét!","Hiba!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(passwordBox.Password))
+            {
+                MessageBox.Show("Nem írta be a jelszavát!", "Hiba!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+    }
+}
---
+++ Zarodolgozat/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zarodolgozat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInButtonClick(object sender, RoutedEventArgs e)
+        {
+            var logInWindow = new LogInWindow();
+            logInWindow.ShowDialog();
+        }
+
+        private void SignInButtonClick(object sender, RoutedEventArgs e)
+        {
+            var signInWindow = new SignInWindow();
+            signInWindow.ShowDialog();
+        }
+
+    }
+}
